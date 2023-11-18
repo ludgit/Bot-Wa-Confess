726e043a8f9b65df3b26bfb4840971d95f11ab8a
LudGithub <149161520+ludgit@users.noreply.github.com>
2023-11-18 21:43:32 +0700

    settings.js

--- bot-wa-confess/settings.js
+++ bot-wa-confess/settings.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 
 global.fotomenfess = fs.readFileSync("./menfess.jpg")
-global.owner = ["6281331720264"]
+global.owner = ["6281331720264"]  // Ganti Nomer Owner Di Sini, Ga diganti jg gpp.
 
 global.help = {
 Menu(pushname){
   return `Halo ${pushname}
 
-Saya adalah bot WhatsApp confess 👋 ketik .confess untuk memulai chat.
+Saya adalah BOT WhatsApp khusus confess 👋 ketik .confess untuk memulai chat.
 `
 }
 }
